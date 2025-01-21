9849a910d3a800bd265befa7875b6394b2318e11
Stoevand-flutterstudios <andrey_stoev@flutterstudios.com>
2025-01-21 19:49:03 +0200

    reeborg maze

--- 03_Treasure_Island/treasure_island.py
+++ 03_Treasure_Island/treasure_island.py
@@ -34,7 +34,7 @@ if choice1 == "left":
     if choice2 == "wait":
         choice3 = input("You arrive at the island unharmed. "
                         "There is house with 3 doors. "
-                        "One red, one yellow and one blue."
+                        "One red, one yellow and one blue. "
                         "Which colour do you choose? ").lower()
         if choice3 == "red":
             print("It's a room full of fire. Game Over.")
---
+++ 06_escaping_the_maze/03_while.py
@@ -0,0 +1,91 @@
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+#
+# number_of_hurdles = 6
+# while number_of_hurdles > 0:
+#     jump()
+#     number_of_hurdles -= 1
+#     print(number_of_hurdles)
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+#
+# while not at_goal():
+#     jump()
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+#
+# while not at_goal():
+#     if front_is_clear():
+#         move()
+#     else:
+#         jump()
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# def jump():
+#     turn_left()
+#     while (wall_on_right()):
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while (front_is_clear()):
+#         move()
+#     turn_left()
+# 
+#
+# while not at_goal():
+#     if front_is_clear():
+#         move()
+#     else:
+#         jump()
