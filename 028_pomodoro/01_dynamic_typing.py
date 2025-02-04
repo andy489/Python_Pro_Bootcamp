14263e18a8daa14431646f95453a818561f5aa8d
Stoevand-flutterstudios <andrey_stoev@flutterstudios.com>
2025-02-04 12:13:30 +0200

    fix

--- 028_pomodoro/01_dynamic_typing.py
+++ 028_pomodoro/01_dynamic_typing.py
@@ -3,9 +3,28 @@
 # Python is a strongly dynamically typed programming language
 a = 3
 print(type(a))
-print(pow(a,3))
+print(pow(a, 3))
 
 a = "dynamic typing"
 print(type(a))
 # print(pow(a,3)) # boom
 
+# Type hints and arrows
+age: int
+name: str
+height: float
+is_human: bool
+
+
+def police_check(age: int) -> bool:
+    if age > 18:
+        can_drive = True
+    else:
+        can_drive = False
+    return can_drive
+
+
+if police_check(12):
+    print("You may pass")
+else:
+    print("Pay a fine.")
