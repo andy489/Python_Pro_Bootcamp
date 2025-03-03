36d105aef3821cc1eb81ae45ab281412dadb33c8
Stoevand-flutterstudios <andrey_stoev@flutterstudios.com>
2025-03-03 16:49:34 +0200

    day_001

---
+++ day_001/band_name_generator.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name generator.")
+
+last_meal = input("What's the last thing you ate?\n").title()
+pet_name = input("What is your pet's name?\n").capitalize()
+
+band_name = pet_name + "'s " + last_meal
+print("Your band name could be \"" + band_name + "\"!")
