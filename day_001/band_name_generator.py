a451ecce9fb7d16a80e7bbefe0757dff2111f468
Andrey Stoev <akstoev@uni-sofia.bg>
2025-03-03 17:28:25 +0200

    Update band_name_generator.py

--- day_001/band_name_generator.py
+++ day_001/band_name_generator.py
@@ -4,4 +4,5 @@ last_meal = input("What's the last thing you ate?\n").title()
 pet_name = input("What is your pet's name?\n").capitalize()
 
 band_name = pet_name + "'s " + last_meal
+
 print("Your band name could be \"" + band_name + "\"!")
