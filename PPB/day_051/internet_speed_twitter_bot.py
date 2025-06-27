76e6504fb3d8c8488396d972350cba8132301595
Stoevand-flutterstudios <andrey_stoev@flutterstudios.com>
2025-06-27 18:07:32 +0300

    ref

--- PPB/day_051/internet_speed_twitter_bot.py
+++ PPB/day_051/internet_speed_twitter_bot.py
@@ -1,5 +1,3 @@
-import os
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -8,6 +6,7 @@ from selenium.common.exceptions import NoSuchElementException
 from time import sleep
 from dotenv import load_dotenv
 import random
+import os
 
 
 class InternetSpeedTwitterBot:
